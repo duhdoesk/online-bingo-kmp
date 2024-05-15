17bf00e7017b5e023b5e98971d479a02ffa1b73d
Eduardo Scaranari <dev.edusc@gmail.com>
2024-05-15 14:28:22 -0300

    :sparkles: feat: added WindowInfo class to manage screen sizes and orientation.

--- composeApp/src/commonMain/kotlin/ui/presentation/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/presentation/home/HomeScreen.kt
@@ -1,52 +1,86 @@
 package ui.presentation.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ui.navigation.Configuration
+import ui.presentation.util.rememberWindowInfo
 
 @Composable
 fun HomeScreen(component: HomeScreenComponent) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+
+    BoxWithConstraints(
+        modifier = Modifier.fillMaxSize(),
+        propagateMinConstraints = true
     ) {
-        Text("Home Screen")
 
-        Button(
-            onClick = { component.navigate(Configuration.CreateScreen) }
-        ) {
-            Text("Create")
-        }
+        val boxWidth = this.maxWidth
+        val boxHeight = this.maxHeight
 
-        Button(
-            onClick = { component.navigate(Configuration.JoinScreen) }
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
         ) {
-            Text("Join")
-        }
 
-        Button(
-            onClick = { component.navigate(Configuration.ThemesScreen) }
-        ) {
-            Text("Themes")
-        }
+            Text("Current Screen")
+            Text("Home Screen")
 
-        Button(
-            onClick = { component.navigate(Configuration.HostScreen) }
-        ) {
-            Text("Host")
-        }
+            val windowInfo = rememberWindowInfo(screenHeight = boxHeight, screenWidth = boxWidth)
 
-        Button(
-            onClick = { component.navigate(Configuration.PlayScreen) }
-        ) {
-            Text("Play")
+            Spacer(Modifier.height(16.dp))
+            Text("Screen Orientation")
+            Text(windowInfo.screenOrientation.toString())
+
+            Spacer(Modifier.height(16.dp))
+            Text("Height Info")
+            Text(windowInfo.screenHeight.toString())
+            Text(windowInfo.screenHeightInfo.toString())
+
+            Spacer(Modifier.height(16.dp))
+            Text("Width Info")
+            Text(windowInfo.screenWidth.toString())
+            Text(windowInfo.screenWidthInfo.toString())
+
+//            Button(
+//                onClick = { component.navigate(Configuration.CreateScreen) }
+//            ) {
+//                Text("Create")
+//            }
+//
+//            Button(
+//                onClick = { component.navigate(Configuration.JoinScreen) }
+//            ) {
+//                Text("Join")
+//            }
+//
+//            Button(
+//                onClick = { component.navigate(Configuration.ThemesScreen) }
+//            ) {
+//                Text("Themes")
+//            }
+//
+//            Button(
+//                onClick = { component.navigate(Configuration.HostScreen) }
+//            ) {
+//                Text("Host")
+//            }
+//
+//            Button(
+//                onClick = { component.navigate(Configuration.PlayScreen) }
+//            ) {
+//                Text("Play")
+//            }
         }
     }
+
 }
