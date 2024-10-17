3a73782a21c17224319a5d9d8c5922525cf99252
Eduardo Scaranari <dev.edusc@gmail.com>
2024-10-17 11:51:22 -0300

    :bug: fix: app was crashing when trying to fetch user entitlements was returning an error.

--- composeApp/src/commonMain/kotlin/domain/billing/BillingHelper.kt
+++ composeApp/src/commonMain/kotlin/domain/billing/BillingHelper.kt
@@ -1,9 +1,12 @@
 package domain.billing
 
 import com.revenuecat.purchases.kmp.Purchases
-import com.revenuecat.purchases.kmp.ktx.awaitCustomerInfo
+import com.revenuecat.purchases.kmp.result.awaitCustomerInfoResult
 
 suspend fun hasActiveEntitlements(): Boolean {
     Purchases.sharedInstance.invalidateCustomerInfoCache()
-    return Purchases.sharedInstance.awaitCustomerInfo().entitlements.active.isNotEmpty()
+    Purchases.sharedInstance.awaitCustomerInfoResult().fold(
+        onSuccess = { customerInfo -> return customerInfo.entitlements.active.isNotEmpty() },
+        onFailure = { return false }
+    )
 }
