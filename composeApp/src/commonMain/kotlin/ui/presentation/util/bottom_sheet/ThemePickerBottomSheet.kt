e2f567e281ed3f23f41d2ed8d730af1dcd7a2d5c
Eduardo Scaranari <dev.edusc@gmail.com>
2024-10-27 22:11:16 -0300

    recycle: refactor: theme picker bottom sheet.

--- composeApp/src/commonMain/kotlin/ui/presentation/util/bottom_sheet/ThemePickerBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/ui/presentation/util/bottom_sheet/ThemePickerBottomSheet.kt
@@ -5,19 +5,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,6 +46,7 @@ fun ThemePickerBottomSheet(
     ModalBottomSheet(
         onDismissRequest = { onDismiss() },
         sheetState = sheetState,
+        windowInsets = WindowInsets.ime,
     ) {
         Column {
             Text(
@@ -64,6 +68,8 @@ fun ThemePickerBottomSheet(
                     )
                 }
             }
+
+            Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.systemBars))
         }
     }
 }
